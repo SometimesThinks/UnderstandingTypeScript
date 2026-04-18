d04d119c16f158630c4eb4019d48db47d941ae41
SometimesThinks <ycw2859@gmail.com>
2026-04-18 22:34:53 +0900

    typescript: Section9 세팅 추가

---
+++ Section9/prj-00-initial-starting-setup/src/app.ts
@@ -0,0 +1,24 @@
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+
+  constructor() {
+    this.templateElement = document.getElementById(
+      'project-input'
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.attatch();
+  }
+  private attatch() {
+    this.hostElement.insertAdjacentElement('afterbegin', this.element);
+  }
+}
+
+const pjtInput = new ProjectInput();
